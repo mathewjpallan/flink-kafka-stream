e570a2335f273d57a9caa97954b211fc6d4f2427
mathewjpallan <mathewjpallan@gmail.com>
2021-12-09 22:36:25 +0530

    Added a metric to count number of events

--- src/main/scala/com/binderror/streaming/CaseHandlerProcessFunction.scala
+++ src/main/scala/com/binderror/streaming/CaseHandlerProcessFunction.scala
@@ -1,12 +1,22 @@
 package com.binderror.streaming
 
 import com.binderror.core.KafkaMsg
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.metrics.Counter
 import org.apache.flink.streaming.api.functions.ProcessFunction
 import org.apache.flink.util.Collector
 
 class CaseHandlerProcessFunction extends ProcessFunction[KafkaMsg, KafkaMsg]{
 
+  private var eventCounter: Counter = null
+
   override def processElement(i: KafkaMsg, context: ProcessFunction[KafkaMsg, KafkaMsg]#Context, collector: Collector[KafkaMsg]): Unit = {
+      eventCounter.inc()
       i.value.split(" ").map(data => collector.collect(KafkaMsg("", data.toLowerCase())))
+  }
+
+  override def open(parameters: Configuration): Unit = {
+    super.open(parameters)
+    eventCounter = getRuntimeContext().getMetricGroup().counter("events_count")
   }
 }
